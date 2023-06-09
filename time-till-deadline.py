c83ee32f165e079c5118437402f3ce27832d7a0e
Vakhor <vakhor@gmail.com>
2023-06-09 12:14:11 +0300

    done deadline task

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -6,5 +6,8 @@ input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
 
-print(datetime.datetime.strptime(deadline, "%d.%m.%Y"))
-print(type(datetime.datetime.strptime(deadline, "%d.%m.%Y")))
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
+
+print(f"Dear user! Time remaining for you goal: {goal} is {time_till.days} days")
