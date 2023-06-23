78d832b4971f9f7de41037a32168659500322a97
Vakhor <vakhor@gmail.com>
2023-06-23 11:07:51 +0300

    exercise 2nd complete

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ product_list = inv_file["Sheet1"]
 
 #exercise 1. supplier with the number of it's products
 product_per_supplier = {}
+total_value_per_supplier = {}
 
 for product_row in range(2, product_list.max_row + 1):
     supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
 
+    #calculation number of products per supplier
     if supplier_name in product_per_supplier:
         current_num_products = product_per_supplier[supplier_name]
         product_per_supplier[supplier_name] = current_num_products + 1
@@ -16,4 +20,15 @@ for product_row in range(2, product_list.max_row + 1):
         print("Adding new supplier")
         product_per_supplier[supplier_name] = 1
 
+    #calculation total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
 print(product_per_supplier)
+print(total_value_per_supplier)
+
+#exercise 2. Total inventory value per supplier
+
